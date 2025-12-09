a6c4ebe9e57d58c81b5608b94a90dbcc7fb9dc10
Daniel Frederick <danandabbey@icloud.com>
2025-12-08 19:50:36 -0500

    update

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -5,6 +5,8 @@ defmodule Identicon do
     |> hash_input()
     |> pick_color()
     |> build_grid()
+    |> filter_odd_squares()
+    
 
   end
 
@@ -20,15 +22,27 @@ defmodule Identicon do
   end
 
   def build_grid(%Identicon.Image{hex: hex} = image) do
-    hex
-    |> Enum.chunk_every(3)
-    |> Enum.map(&mirror_row/1)
+    grid =
+      hex
+      |> Enum.chunk_every(3)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten()
+      |> Enum.with_index()
+
+    %Identicon.Image{image | grid: grid}
   end
 
   def mirror_row([first, second | _tail] = row) do
     row ++ [second, first]
   end
 
+  def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
+    grid = Enum.filter grid, fn({code, _index}) ->
+      rem(code, 2) == 0
+    end
+
+    %Identicon.Image{ image | grid: grid }
+  end
 
 
 end
